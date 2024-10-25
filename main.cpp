1a1e5491a878ce7296992f74dadb23b7a73cfb60
euan <euan.williams@onebigcircle.co.uk>
2024-10-25 23:42:22 +0100

    added ability to query the database

--- main.cpp
+++ main.cpp
@@ -16,5 +16,8 @@
 int main() {
   DatabaseInterface myDatabase = DatabaseInterface("test.db");
   myDatabase.connect_to_database();
+  myDatabase.execute_query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);");
+  /*myDatabase.execute_query("INSERT INTO users (name, age) VALUES ('euan', 25);");*/
+  myDatabase.execute_query("SELECT * FROM users;");
   return 0;
 }
--- src/database.cpp
+++ src/database.cpp
@@ -6,7 +6,7 @@
  *
  * @ingroup image-store
  *
- * @author Euan Williams
+ * @author Euan williams
  *
  */
 #include <iostream>
@@ -15,31 +15,60 @@
 
 class DatabaseInterface {
   private:
-    sqlite3* database;
 
   public:
     std::string database_name;
+    sqlite3* database;
     DatabaseInterface(std::string database_name) {
       this->database_name = database_name;
     }
-
-    void connect_to_database() {
-      sqlite3* database;
-      int error = sqlite3_open(database_name.c_str(), &database);
-      this->database = database;
-
-      if (error) {
-          std::cerr << "Can't open database: " << sqlite3_errmsg(database) << std::endl;
-      } else {
-          std::cout << "Opened database " << database_name << " successfully" << std::endl;
-      }
-      sqlite3_close(database);
-    };
-
-    void query() {
-      
+  
+    bool connect_to_database() {
+        int error = sqlite3_open(database_name.c_str(), &database);
+        if (error) {
+            std::cerr << "Can't open database: " << sqlite3_errmsg(database) << std::endl;
+            return false;
+        } else {
+            std::cout << "Opened database " << database_name << " successfully" << std::endl;
+            return true;
+        }
     }
 
+    // query execution method
+    void execute_query(const std::string &query) {
+        char* error_message = nullptr;
+        
+        int result = sqlite3_exec(database, query.c_str(), callback, nullptr, &error_message);
+        
+        if (result != SQLITE_OK) {
+            std::cerr << "error executing query: " << error_message << std::endl;
+            sqlite3_free(error_message);
+        } else {
+            std::cout << "query executed successfully" << std::endl;
+        }
+    };
+
+    // Method to disconnect from the database
+    void disconnect() {
+        if (database) {
+            sqlite3_close(database);
+            std::cout << "Database closed successfully." << std::endl;
+        }
+    }
+
+    ~DatabaseInterface() {
+        disconnect();
+    }
+
+  private:
+    // Callback function for query results
+    static int callback(void* not_used, int argc, char** argv, char** azColName) {
+        for (int i = 0; i < argc; i++) {
+            std::cout << azColName[i] << ": " << (argv[i] ? argv[i] : "NULL") << std::endl;
+        }
+        std::cout << std::endl;
+        return 0;
+    }
 };
 
 
