d29186606136a6938fe0334cdcdf37bf4d062cdb
euan <euan.williams@onebigcircle.co.uk>
2024-10-26 11:58:49 +0100

    added argument parser

--- main.cpp
+++ main.cpp
@@ -11,13 +11,16 @@
 */
 #include <iostream>
 
-#include "src/database.cpp"
+#include "src/database.h"
+#include "src/argument_parser.h"
 
-int main() {
-  DatabaseInterface myDatabase = DatabaseInterface("test.db");
-  myDatabase.connect_to_database();
-  myDatabase.execute_query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);");
+
+int main(int argc, char* argv[]) {
+  Arguments opt = parse_args(argc, argv);
+  /*DatabaseInterface myDatabase = DatabaseInterface("test.db");*/
+  /*myDatabase.connect_to_database();*/
+  /*myDatabase.execute_query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);");*/
   /*myDatabase.execute_query("INSERT INTO users (name, age) VALUES ('euan', 25);");*/
-  myDatabase.execute_query("SELECT * FROM users;");
+  /*myDatabase.execute_query("SELECT * FROM users;");*/
   return 0;
 }
---
+++ src/argument_parser.h
@@ -0,0 +1,48 @@
+/**
+ *
+ * @file argument_paraser.cpp
+ *
+ * @brief for parsing command line arguments
+ *
+ * @ingroup image-store
+ *
+ * @author Euan Williams
+ *
+*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+class Arguments {
+  public:
+    std::string mode;
+};
+
+// Parses a single arg
+std::string parse_arg(std::vector<std::string> raw_args, std::string key) {
+    auto index = find(raw_args.begin(), raw_args.end(), key);
+    int arg_position = std::distance(raw_args.begin(), index);
+
+    std::string value = raw_args[arg_position + 1];
+
+    if (index == raw_args.end()) {
+      std::cout << "Not found." << std::endl;
+    }
+    else {
+      std::cout << " - Found argument '" << key << "' value: '" << value << "'" << std::endl;
+    }
+    return value;
+}
+
+Arguments parse_args(int argc, char* argv[]) {
+    std::cout << "Parsing args..." << std::endl;
+
+    // Convert argv to a vector of strings for easier handling
+    std::vector<std::string> args(argv, argv + argc);
+
+    Arguments parsed_args;
+
+    parsed_args.mode = parse_arg(args, "--mode");
+    return parsed_args;
+};
