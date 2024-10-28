b3f088e0fff923f676fba778fda17072ce068a42
euan <euan.williams@onebigcircle.co.uk>
2024-10-28 17:12:04 +0000

    added import

---
+++ src/import.cpp
@@ -0,0 +1,27 @@
+/**
+ *
+ * @file import.cpp
+ *
+ * @brief for importing images to the database
+ *
+ * @ingroup image-store
+ *
+ * @author Euan Williams
+ *
+*/
+
+#include <iostream>
+
+#include "database.h"
+
+class ImageImporter {
+  DatabaseInterface database;
+  
+  public:
+    ImageImporter(const std::string& database_file)
+        : database(database_file) // Correctly initialize 'database'
+    {
+        // Other initialization code, if needed
+    }
+};
+
