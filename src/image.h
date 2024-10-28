c4cfdccc601049d53266d85257b405f766681ebd
Euan Williams <euan.williams01061999@gmail.com>
2024-10-28 19:35:29 +0000

    added image.h

---
+++ src/image.h
@@ -0,0 +1,21 @@
+/**
+ *
+ * @file argument_paraser.cpp
+ *
+ * @brief for parsing command line arguments
+ *
+ * @ingroup image-store
+ *
+ * @author Euan Williams
+ *
+*/
+#pragma once
+
+#include <iostream>
+
+class Image {
+  public:
+    int id;
+    
+}
+
